862456c6a4e47cc6c2a6226ce86e73de4e42c6f7
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-23 11:45:43 -0300

    todo txtaaaaa

--- src/components/sumary.tsx
+++ src/components/sumary.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react'
 
 function Summary({ sum }) {
-  const [summary, setSummary] = useState<any>(null)
+  // const [summary, setSummary] = useState<any>(null)
 
-  // const { data } = useQuery<any>({
-  //   queryKey: ['summary'],
-  //   queryFn: getSummary,
-  // })
-  async function Get() {
-    const response = await fetch('http://localhost:3333/summary')
-    //   const response = await fetch('https://in-orbit-nine.vercel.app/summary')
-    const data = await response.json()
-    console.log(data)
-    setSummary(data)
-    return data
-  }
+  // // const { data } = useQuery<any>({
+  // //   queryKey: ['summary'],
+  // //   queryFn: getSummary,
+  // // })
+  // async function Get() {
+  //   const response = await fetch('http://localhost:3333/summary')
+  //   //   const response = await fetch('https://in-orbit-nine.vercel.app/summary')
+  //   const data = await response.json()
+  //   console.log(data)
+  //   setSummary(data)
+  //   return data
+  // }
 
-  useEffect(() => {
-    Get()
-  }, [])
+  // useEffect(() => {
+  //   Get()
+  // }, [])
 
   return (
     <>
